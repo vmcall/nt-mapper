5e1411e65fbe906c53e091404b2d947b31d72d2a
Admin <Admin@ADMINISTRATOR>
2017-10-22 22:59:20 +0200

    fixed leak

--- mapper/loadlibrary.cpp
+++ mapper/loadlibrary.cpp
@@ -14,28 +14,32 @@ bool injection::loadlibrary::inject(std::string& buffer)
 		return false;
 	}
 
-	// WRITE LIBRARY PATH TO PROCESS MEMORY
-	if (!this->process.write_raw_memory(buffer.data(), buffer.length(), path_pointer))
+	do
 	{
-		logger::log_error("Failed to write library path!");
-		return false;
-	}
+		// WRITE LIBRARY PATH TO PROCESS MEMORY
+		if (!this->process.write_raw_memory(buffer.data(), buffer.length(), path_pointer))
+		{
+			logger::log_error("Failed to write library path!");
+			break;
+		}
 
-	// FIND LOADLIBRARY FUNCTION POINTER
-	auto module_handle = GetModuleHandle(L"kernel32.dll");
-	auto function_pointer = reinterpret_cast<uintptr_t>(GetProcAddress(module_handle, "LoadLibraryA"));
+		// FIND LOADLIBRARY FUNCTION POINTER
+		auto module_handle = GetModuleHandle(L"kernel32.dll");
+		auto function_pointer = reinterpret_cast<uintptr_t>(GetProcAddress(module_handle, "LoadLibraryA"));
 
-	// EXECUTE LOADLIBRARY IN REMOTE PROCESS
-	auto thread_handle = safe_handle(this->process.create_thread(function_pointer, path_pointer));
+		// EXECUTE LOADLIBRARY IN REMOTE PROCESS
+		auto thread_handle = safe_handle(this->process.create_thread(function_pointer, path_pointer));
 
-	if (!thread_handle)
-	{
-		logger::log_error("Failed to create thread!");
-		return false;
-	}
+		if (!thread_handle)
+		{
+			logger::log_error("Failed to create thread!");
+			break;
+		}
 
-	// WAIT FOR THREAD TO FINISH
-	WaitForSingleObject(thread_handle.get_handle(), INFINITE);
+		// WAIT FOR THREAD TO FINISH
+		WaitForSingleObject(thread_handle.get_handle(), INFINITE);
+	} while (false);
+	
 
 	// FREE LIBRARY PATH FROM PROCESS MEMORY
 	this->process.free_memory(path_pointer);
